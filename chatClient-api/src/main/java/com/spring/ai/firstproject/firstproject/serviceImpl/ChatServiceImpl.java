6f1c70045bbfba968218e97005e1225203666f57
Ravikiran Chavan <varsha.khese@hindindustec.com>
2026-07-16 00:51:26 +0530

    fluent prompts controller added

--- chatClient-api/src/main/java/com/spring/ai/firstproject/firstproject/serviceImpl/ChatServiceImpl.java
+++ chatClient-api/src/main/java/com/spring/ai/firstproject/firstproject/serviceImpl/ChatServiceImpl.java
@@ -22,14 +22,13 @@ public class ChatServiceImpl implements ChatService {
 		this.chatClient = chatClient.build();
 	}
 
-	 @Value("classpath:/prompts/userPrompts-message.st")
-	 private Resource userPrompt;
-	 
+	@Value("classpath:/prompts/userPrompts-message.st")
+	private Resource userPrompt;
+
 	@Override
 	public String chat(String query) {
 
-		String content = 
-				chatClient
+		String content = chatClient
 				.prompt()// generate more specific prompt
 				.user(query)
 				.system("as an experts in cricket")
@@ -42,23 +41,24 @@ public class ChatServiceImpl implements ChatService {
 
 	@Override
 	public Tutorial entityResponse(String query) {
-		
-		Tutorial tutorial = chatClient.prompt(query)
-		.call()
-		.entity(Tutorial.class);
-		
+
+		Tutorial tutorial = chatClient
+				.prompt(query)
+				.call()
+				.entity(Tutorial.class);
+
 		System.out.println(tutorial);
 		return tutorial;
 	}
 
 	@Override
 	public List<Tutorial> listOfEntityResponse(String query) {
-		List<Tutorial> tutorials = 
-				chatClient
+		List<Tutorial> tutorials = chatClient
 				.prompt(query)
 				.call()
-				.entity(new ParameterizedTypeReference<List<Tutorial>>() {//when return data in multiple list then use ParameterizedTypeReference
-				});
+				.entity(new ParameterizedTypeReference<List<Tutorial>>() {});// when return data in multiple list then use
+																			// ParameterizedTypeReference
+				
 
 		System.out.println(tutorials);
 
@@ -68,42 +68,40 @@ public class ChatServiceImpl implements ChatService {
 
 	@Override
 	public String promptTemplate(String query) {
-		
+
 		Prompt prompt = new Prompt(query);
-		
+
 		String result = chatClient
-		.prompt(query)
-		.user(query)
-		.system("Give me in dotNet")// provide more specific prompt
-		.call()
-		.content();
-		
+				.prompt(query)
+				.user(query)
+				.system("Give me in dotNet")// provide more specific prompt
+				.call()
+				.content();
+
 		return result;
 	}
 
 	@Override
 	public String dynamicQueryPrompt(String query) {
 		String dynamicVar = "As an expert in coding and programing.Always write program in java. now reply for this question: {keyQuery}";
-		
+
 		String result = chatClient
 				.prompt()
-				.user(x->x.text(dynamicVar).param("keyQuery", query))
+				.user(x -> x.text(dynamicVar).param("keyQuery", query))
 				.call()
 				.content();
-				
+
 		return result;
 	}
 
 	@Override
 	public String fluentPrompt(String query) {
-		String result = chatClient.prompt().user(x->x.text(userPrompt).params(
-				"name", "Ravikiran",
-                "technology", "Spring AI",
-                "experience", 3,
-                "question", "Explain PromptTemplate"
-		))
-		.call()
-		.content();
+		String result = chatClient
+				.prompt()
+				.user(x -> x.text(userPrompt).params(Map.of("name", "Ravikiran", "technology",
+						"Spring AI", "experience", 3, "question", "Explain PromptTemplate")))
+				.call()
+				.content();
 		return result;
 	}
 
