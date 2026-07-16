79428d9ef4deaff2798a8f92c9a1eb2963602ae7
Ravikiran Chavan <varsha.khese@hindindustec.com>
2026-07-17 00:49:15 +0530

    Add Advisor Cross cutting concept

--- chatClient-api/src/main/java/com/spring/ai/firstproject/firstproject/service/ChatService.java
+++ chatClient-api/src/main/java/com/spring/ai/firstproject/firstproject/service/ChatService.java
@@ -2,7 +2,11 @@ package com.spring.ai.firstproject.firstproject.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.spring.ai.firstproject.firstproject.entity.Tutorial;
+
+import reactor.core.publisher.Flux;
 
 public interface ChatService {
 
@@ -17,6 +21,12 @@ public interface ChatService {
 	String dynamicQueryPrompt(String query);
 
 	String fluentPrompt(String query);
+
+	String simpleLoggerAdvisor(String query);
+
+	String safeGardAdvisor(String query);
+
+	Flux<String> streamingConcept(String query);
 
 }
 
