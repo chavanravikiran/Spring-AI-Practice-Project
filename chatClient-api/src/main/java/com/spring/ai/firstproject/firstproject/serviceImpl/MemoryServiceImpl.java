689940e8917e7c1b6c90dda3711a5bfdcb6e5468
Ravikiran Chavan <varsha.khese@hindindustec.com>
2026-07-19 00:33:05 +0530

    chat history save in database using jdbcChatMemoryRepository

--- chatClient-api/src/main/java/com/spring/ai/firstproject/firstproject/serviceImpl/MemoryServiceImpl.java
+++ chatClient-api/src/main/java/com/spring/ai/firstproject/firstproject/serviceImpl/MemoryServiceImpl.java
@@ -1,50 +1,37 @@
 package com.spring.ai.firstproject.firstproject.serviceImpl;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
-import com.spring.ai.firstproject.firstproject.entity.Tutorial;
-import com.spring.ai.firstproject.firstproject.service.ChatService;
 import com.spring.ai.firstproject.firstproject.service.MemoryService;
-
 import reactor.core.publisher.Flux;
 
 @Service
 public class MemoryServiceImpl implements MemoryService {
 
-	public ChatClient chatClient;
+	private final ChatClient chatClient;
 
-	public MemoryServiceImpl(ChatClient.Builder chatClient) {
-		this.chatClient = chatClient.build();
+	public MemoryServiceImpl(ChatClient chatClient) {
+		this.chatClient = chatClient;
 	}
 
-    @Override
-    public String memory(String conversationId, String query) {
+	@Override
+	public String memory(String conversationId, String query) {
 
-        return chatClient.prompt()
-                .user(query)
-                .advisors(advisor -> advisor.param("conversationId", conversationId))
-                .call()
-                .content();
-    }
+		return chatClient.prompt().user(query)
+//                .advisors(advisor -> advisor.param("conversationId", conversationId))
+				.advisors(advisor -> advisor.param("chat_memory_conversation_id", conversationId)).call().content();
+	}
 
-    @Override
-    public Flux<String> stream(String conversationId, String query) {
+	@Override
+	public Flux<String> stream(String conversationId, String query) {
 
-        return chatClient.prompt()
-                .user(query)
-                .advisors(advisor -> advisor.param("conversationId", conversationId))
-                .stream()
-                .content();
-    }
+		return chatClient.prompt().user(query)
+				.advisors(advisor -> advisor.param("chat_memory_conversation_id", conversationId)).stream().content();
+	}
+
+	@Override
+	public String memorySession(String userId,  String query) {
+		return chatClient.prompt().user(query)
+				.advisors(advisor -> advisor.param("chat_memory_conversation_id", userId)).call().content();
+	}
 }
-
